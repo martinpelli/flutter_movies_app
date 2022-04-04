bddd8a3b727a566fe62702657adc376c2925524d
Martin Pellicer <martinpelli@outlook.com>
2022-04-04 08:48:09 -0300

    Home Screen with Card swiper created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movies_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Movies on cinemas'),
+          elevation: 0,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: [CardSwiper()],
+        ));
+  }
+}
