13a15477cbe3c2b43f6a395e4928eae7018a0569
Martin Pellicer <martinpelli@outlook.com>
2022-04-04 22:40:15 -0300

    Added casting cards to movie details, frontend first layer finished

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,11 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies_app/widgets/casting_cards.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(child: Text("Details Screen")),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            _PosterAndTitle(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            CastinCards()
+          ]))
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          padding: EdgeInsets.only(bottom: 10),
+          color: Colors.black12,
+          child: Text('movie.title', style: TextStyle(fontSize: 16)),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/200x300'),
+            height: 150,
+          ),
+        ),
+        SizedBox(width: 20),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            'movie.title',
+            style: textTheme.headline5,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          Text('movie.originalTitle',
+              style: textTheme.subtitle1, overflow: TextOverflow.ellipsis),
+          Row(
+            children: [
+              Icon(Icons.star_outline, size: 15, color: Colors.grey),
+              SizedBox(width: 5),
+              Text('movie.voteAverage', style: textTheme.caption)
+            ],
+          )
+        ]),
+      ]),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'asdasdjasdhja hdjahsdjhasjkdhba jkhsdjashd lahsdlaksjd akjsdlka hjnsdkajsdasdkjaskñdjaslñkdjañlskdjaskldjaskldjaskdljaskd.',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
       ),
     );
   }
